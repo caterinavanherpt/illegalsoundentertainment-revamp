1b84a2d5c039357100961df4407238326a5ea77b
Caterina <cvanherpt@deloitte.ca>
2024-06-15 17:19:31 -0400

    adding form

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,7 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-      </footer>
-    </>
+    <footer className={styles.footer}>
+    </footer>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
+import Contact  from '@components/Contact';
+
 export default function Home() {
   return (
     <>
-      <h1>This is the home page, test </h1>
+      <Contact/>
     </>
   )
 }
